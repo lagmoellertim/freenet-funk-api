03c8c7394fda38c076151531af232095400c46ef
Tim-Luca Lagmöller <lagmoellertim@gmail.com>
2019-05-08 23:41:09 +0200

    Added Setuptools for easy installing

---
+++ funkapi/__init__.py
@@ -0,0 +1,1 @@
+from funkapi.api import FunkAPI
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='funkapi',
+    version='0.1',
+    install_requires=requirements,
+    license='MIT License',
+    author='Tim-Luca Lagmöller',
+    author_email='hello@lagmoellertim.de',
+    description='Reverse engineered API of Freenet FUNK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/lagmoellertim/freenet-funk-api',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
