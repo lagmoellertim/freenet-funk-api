f9cd0d698fcc1c8d787fe0f4830da07c87c61fcf
Florian Beier <florian@beier.xyz>
2019-05-11 15:33:29 +0200

    Fixed translation
    Fixed the translation of the German word "Tarif" by replacing all instances of "Tariff" / "tariff" with "Plan" / "plan".

--- funkapi/api.py
+++ funkapi/api.py
@@ -63,7 +63,7 @@ class FunkAPI:
     # DATA
     def getData(self, refresh=False):
         json = {"operationName": "CustomerForDashboardQuery", "variables": {},
-                "query": "query CustomerForDashboardQuery {\n  me {\n    ...CustomerForDashboardFragment\n    __typename\n  }\n}\n\nfragment CustomerForDashboardFragment on Customer {\n  id\n  details {\n    ...DetailsFragment\n    __typename\n  }\n  customerProducts {\n    ...ProductFragment\n    __typename\n  }\n  __typename\n}\n\nfragment DetailsFragment on Details {\n  firstName\n  lastName\n  dateOfBirth\n  contactEmail\n  __typename\n}\n\nfragment ProductFragment on FUNKCustomerProduct {\n  id\n  state\n  paymentMethods {\n    ...PaymentMethodFragment\n    __typename\n  }\n  mobileNumbers {\n    ...MobileNumberFragment\n    __typename\n  }\n  sims {\n    ...SIMFragment\n    __typename\n  }\n  tariffs: tariffCustomerProductServices {\n    ...TariffFragment\n    __typename\n  }\n  __typename\n}\n\nfragment PaymentMethodFragment on PaymentMethod {\n  id\n  state\n  approvalChallenge {\n    approvalURL\n    __typename\n  }\n  agreement {\n    state\n    payerInfo {\n      payerID\n      email\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment MobileNumberFragment on MobileNumberCPS {\n  id\n  number\n  state\n  usage {\n    usedDataPercentage\n    __typename\n  }\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    __typename\n  }\n  ... on MNPImportCustomerProductService {\n    otherProviderShortcut\n    otherProviderCustomName\n    otherContract {\n      contractType\n      mobileNumber\n      mobileNumberIsVerified\n      __typename\n    }\n    mnpInfos {\n      confirmedPortingDate\n      lastPortingResult\n      problemCode\n      problemReason\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment SIMFragment on SIMCustomerProductService {\n  id\n  networkState\n  state\n  iccid\n  delivery {\n    state\n    trackingDetails {\n      stateId\n      stateLabel\n      trackingURL\n      __typename\n    }\n    deliveryProvider\n    address {\n      city\n      additionalInfo\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment TariffFragment on TariffCustomerProductService {\n  id\n  booked\n  starts\n  state\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    follower {\n      id\n      label\n      __typename\n    }\n    marketingInfo {\n      name\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"}
+                "query": "query CustomerForDashboardQuery {\n  me {\n    ...CustomerForDashboardFragment\n    __typename\n  }\n}\n\nfragment CustomerForDashboardFragment on Customer {\n  id\n  details {\n    ...DetailsFragment\n    __typename\n  }\n  customerProducts {\n    ...ProductFragment\n    __typename\n  }\n  __typename\n}\n\nfragment DetailsFragment on Details {\n  firstName\n  lastName\n  dateOfBirth\n  contactEmail\n  __typename\n}\n\nfragment ProductFragment on FUNKCustomerProduct {\n  id\n  state\n  paymentMethods {\n    ...PaymentMethodFragment\n    __typename\n  }\n  mobileNumbers {\n    ...MobileNumberFragment\n    __typename\n  }\n  sims {\n    ...SIMFragment\n    __typename\n  }\n  plans: planCustomerProductServices {\n    ...PlanFragment\n    __typename\n  }\n  __typename\n}\n\nfragment PaymentMethodFragment on PaymentMethod {\n  id\n  state\n  approvalChallenge {\n    approvalURL\n    __typename\n  }\n  agreement {\n    state\n    payerInfo {\n      payerID\n      email\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment MobileNumberFragment on MobileNumberCPS {\n  id\n  number\n  state\n  usage {\n    usedDataPercentage\n    __typename\n  }\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    __typename\n  }\n  ... on MNPImportCustomerProductService {\n    otherProviderShortcut\n    otherProviderCustomName\n    otherContract {\n      contractType\n      mobileNumber\n      mobileNumberIsVerified\n      __typename\n    }\n    mnpInfos {\n      confirmedPortingDate\n      lastPortingResult\n      problemCode\n      problemReason\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment SIMFragment on SIMCustomerProductService {\n  id\n  networkState\n  state\n  iccid\n  delivery {\n    state\n    trackingDetails {\n      stateId\n      stateLabel\n      trackingURL\n      __typename\n    }\n    deliveryProvider\n    address {\n      city\n      additionalInfo\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment PlanFragment on PlanCustomerProductService {\n  id\n  booked\n  starts\n  state\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    follower {\n      id\n      label\n      __typename\n    }\n    marketingInfo {\n      name\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"}
 
         if self.data is None or refresh:
             req = requests.post(self.API_ENDPOINT, json=json,
@@ -85,17 +85,17 @@ class FunkAPI:
     def getOrderedProducts(self, refreshData=False):
         return self.getData(refresh=refreshData)["data"]["me"]["customerProducts"]
 
-    def getCurrentTariff(self, refreshData=False):
-        return self.getData(refresh=refreshData)["data"]["me"]["customerProducts"][0]["tariffs"][-1]
+    def getCurrentPlan(self, refreshData=False):
+        return self.getData(refresh=refreshData)["data"]["me"]["customerProducts"][0]["plans"][-1]
 
     # TARIFFS
-    def orderTariff(self, tariffID, productID=None, refreshData=True):
+    def orderPlan(self, planID, productID=None, refreshData=True):
         if productID is None:
             productID = self.getOrderedProducts()[0]["id"]
 
-        json = {"operationName": "AddTariffToProductMutation",
-                "variables": {"productID": productID, "tariffID": str(tariffID)},
-                "query": "mutation AddTariffToProductMutation($productID: String!, $tariffID: String!) {\n  tariffAddToCustomerProduct(customerProductId: $productID, productServiceId: $tariffID) {\n    ...TariffFragment\n    __typename\n  }\n}\n\nfragment TariffFragment on TariffCustomerProductService {\n  id\n  booked\n  starts\n  state\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    follower {\n      id\n      label\n      __typename\n    }\n    marketingInfo {\n      name\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"}
+        json = {"operationName": "AddPlanToProductMutation",
+                "variables": {"productID": productID, "planID": str(planID)},
+                "query": "mutation AddPlanToProductMutation($productID: String!, $planID: String!) {\n  planAddToCustomerProduct(customerProductId: $productID, productServiceId: $planID) {\n    ...PlanFragment\n    __typename\n  }\n}\n\nfragment PlanFragment on PlanCustomerProductService {\n  id\n  booked\n  starts\n  state\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    follower {\n      id\n      label\n      __typename\n    }\n    marketingInfo {\n      name\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"}
 
         req = requests.post(self.API_ENDPOINT, json=json,
                             headers={
@@ -107,11 +107,11 @@ class FunkAPI:
 
         return req.json()
 
-    def removeProduct(self, personalTariffID, refreshData=True):
+    def removeProduct(self, personalPlanID, refreshData=True):
 
-        json = {"operationName": "TerminateTariffMutation",
-                "variables": {"tariffID": personalTariffID},
-                "query": "mutation TerminateTariffMutation($tariffID: String!) {\n  tariffTerminate(customerProductServiceId: $tariffID) {\n    ...TariffFragment\n    __typename\n  }\n}\n\nfragment TariffFragment on TariffCustomerProductService {\n  id\n  booked\n  starts\n  state\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    follower {\n      id\n      label\n      __typename\n    }\n    marketingInfo {\n      name\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"}
+        json = {"operationName": "TerminatePlanMutation",
+                "variables": {"planID": personalPlanID},
+                "query": "mutation TerminatePlanMutation($planID: String!) {\n  planTerminate(customerProductServiceId: $planID) {\n    ...PlanFragment\n    __typename\n  }\n}\n\nfragment PlanFragment on PlanCustomerProductService {\n  id\n  booked\n  starts\n  state\n  productServiceId\n  productServiceInfo {\n    id\n    label\n    follower {\n      id\n      label\n      __typename\n    }\n    marketingInfo {\n      name\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n"}
 
         req = requests.post(self.API_ENDPOINT, json=json,
                             headers={
@@ -123,16 +123,16 @@ class FunkAPI:
 
         return req.json()
 
-    def order1GBTariff(self, **kwargs):
-        return self.orderTariff(9, **kwargs)
+    def order1GBPlan(self, **kwargs):
+        return self.orderPlan(9, **kwargs)
 
-    def orderUnlimitedTariff(self, **kwargs):
-        return self.orderTariff(8, **kwargs)
+    def orderUnlimitedPlan(self, **kwargs):
+        return self.orderPlan(8, **kwargs)
 
     def startPause(self, **kwargs):
-        return self.orderTariff(42, **kwargs)
+        return self.orderPlan(42, **kwargs)
 
-    def stopLatestTariff(self, productIndex=0, **kwargs):
-        personalTariffID = self.getOrderedProducts(
-        )[productIndex]["tariffs"][-1]["id"]
-        self.removeProduct(personalTariffID, **kwargs)
+    def stopLatestPlan(self, productIndex=0, **kwargs):
+        personalPlanID = self.getOrderedProducts(
+        )[productIndex]["plans"][-1]["id"]
+        self.removeProduct(personalPlanID, **kwargs)
